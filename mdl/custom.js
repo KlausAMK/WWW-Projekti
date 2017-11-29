c02aae9f4651a034779a552a120c3c7c83b6f4e8
Klaus <klaus.jokinen@edu.turkuamk.fi>
2017-11-29 13:29:42 +0200

    Modified custom.js scrip to universal + shortened it.

--- mdl/custom.js
+++ mdl/custom.js
@@ -1,35 +1,11 @@
 jQuery(function () {
-    $('a[href="#scroll-tab-1"]').on('click', function () {
-        $('a.mdl-layout__tab').removeClass('is-active');
-        $('.mdl-layout__tab-panel').removeClass('is-active');
-        $('#scroll-tab-1').addClass('is-active');
-        $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
-    });
-});
+    $('.mdl-navigation').children().each(function () {
+        $(this).on('click', function () {
+            $('a.mdl-layout__tab').removeClass('is-active');
+            $('.mdl-layout__tab-panel').removeClass('is-active');
+            $($(this).attr('href')).addClass('is-active');
+            $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
 
-jQuery(function () {
-    $('a[href="#scroll-tab-2"]').on('click', function () {
-        $('a.mdl-layout__tab').removeClass('is-active');
-        $('.mdl-layout__tab-panel').removeClass('is-active');
-        $('#scroll-tab-2').addClass('is-active');
-        $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
-    });
-});
-
-jQuery(function () {
-    $('a[href="#scroll-tab-3"]').on('click', function () {
-        $('a.mdl-layout__tab').removeClass('is-active');
-        $('.mdl-layout__tab-panel').removeClass('is-active');
-        $('#scroll-tab-3').addClass('is-active');
-        $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
-    });
-});
-
-jQuery(function () {
-    $('a[href="#scroll-tab-4"]').on('click', function () {
-        $('a.mdl-layout__tab').removeClass('is-active');
-        $('.mdl-layout__tab-panel').removeClass('is-active');
-        $('#scroll-tab-4').addClass('is-active');
-        $('.mdl-layout__drawer, .mdl-layout__obfuscator').removeClass('is-visible');
+        });
     });
 });
